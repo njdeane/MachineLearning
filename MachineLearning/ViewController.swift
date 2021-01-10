6111cd696108ab3da97c09f6086eead656375b7e
njdeane <njdeane7@gmail.com>
2021-01-10 14:33:25 +1000

    Add imagePicker func with delegates: Add VNCoreML object with request handler.

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -13,16 +13,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imageView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+//        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userImage = info[.originalImage] as? UIImage {
+            imageView.image = userImage
+            
+            guard let ciImage = CIImage(image: userImage) else {
+                fatalError("Could not convert to CI Image")
+            }
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading coreML model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+        }
+    }
+    
+    
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
 }
 
